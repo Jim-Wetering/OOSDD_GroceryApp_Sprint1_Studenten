928e309ff8d560d02ba3c39427cc0337334f4bfd
Jim-Wetering <145760054+Jim-Wetering@users.noreply.github.com>
2025-09-11 10:27:51 +0200

    UC3 completed

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -26,7 +26,7 @@ namespace Grocery.Core.Data.Repositories
         }
         public List<Product> GetAll()
         {
-            return []; //wijzig dit in de gemaakte lijst uit de constructo
+            return products; //wijzig dit in de gemaakte lijst uit de constructo
         }
 
         public Product? Get(int id)
